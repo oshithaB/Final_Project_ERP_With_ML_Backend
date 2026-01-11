74961be5bd9c31c83a5f9215c3069530f91075e2
oshithaB <oshithaB@github.com>
2026-01-11 07:44:20 +0000

    Update logic in retrain.py

--- ML predictions/scripts/retrain.py
+++ ML predictions/scripts/retrain.py
@@ -23,3 +23,4 @@ if __name__ == "__main__":
     print("\nRetraining Process Complete.")
     print("New models saved to 'models/' directory.")
     print("New predictions saved to 'outputs/' directory.")
+
